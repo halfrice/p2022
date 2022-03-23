3dd0ea503330d72b7fe2ec25d4a68314d78f2768
halfrice <halfrice@protonmail.com>
2022-03-23 13:16:02 -0700

    Add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div>
+    <Layout>
       <h1>P2022</h1>
-    </div>
+    </Layout>
   )
 }
 
